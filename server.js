a79a1df0c0284d1316e49a41add6ca60f10bd24f
Marek Praski <marekpraski@prgw.com.pl>
2023-01-06 20:43:39 +0100

    dodatkowa strona html z dodatkowym post i get

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){res.sendFile(__dirname + "/index.html");});
-app.get("/about", function(req1, res1){res1.send("this is my short bio");});
+app.get("/bmiC", function(req, res){res.sendFile(__dirname + "/bmiCalculator.html")});
 
 app.post("/", rootPost);
+app.post("/bmiC", bmiPost);
 
 function rootPost(req, res){
     var nr1 = Number(req.body.nr1);
@@ -15,6 +16,13 @@ function rootPost(req, res){
     var r = nr1 + nr2;
     // próba wysłania przez res.send liczby generuje błąd serwera, odbiera ją jako kod błędu
     res.send("result is " + r);
+}
+
+function bmiPost(req, res){
+    let w = parseFloat(req.body.weight);
+    let h = parseFloat(req.body.heigth);
+    let bmi = Math.round(1000 * w / h) / 1000;
+    res.send("bmi is " + bmi);
 }
 
 app.listen(3000, function(){console.log("Listening on port 3000 ...");});
