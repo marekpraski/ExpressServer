98acf244e55fc87bc2e5402ec911ddb67dba3da0
Marek Praski <marekpraski@prgw.com.pl>
2023-01-06 16:18:25 +0100

    zamiast prostego tekstu przesyłam przez serwer całą stronę html

--- server.js
+++ server.js
@@ -3,16 +3,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", rootAnswer);
-app.listen(3000, messageWhenListening);
+app.listen(3000, function(){console.log("Listening on port 3000 ...");});
 
 app.get("/about", aboutAnswer);
 
-function messageWhenListening(){
-    console.log("Listening on port 3000 ...");
-}
-
 function rootAnswer(req, res){
-    res.send("hi there, it is me");
+    res.sendFile(__dirname + "/index.html");
 }
 
 function aboutAnswer(req, res){
