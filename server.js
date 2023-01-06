6ecb210d98bfd371211d87a337366c1d45647645
Marek Praski <marekpraski@prgw.com.pl>
2023-01-06 19:48:37 +0100

    prosty kalkulator czyta dwie liczby z pól tekstowych, serwer na backendzie przechwytuje je, dodaje, backend wysyła do frontendu wynik

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", rootAnswer);
+app.get("/", function(req, res){res.sendFile(__dirname + "/index.html");});
+app.get("/about", function(req1, res1){res1.send("this is my short bio");});
+
+app.post("/", rootPost);
+
+function rootPost(req, res){
+    var nr1 = Number(req.body.nr1);
+    var nr2 = Number(req.body.nr2);
+    var r = nr1 + nr2;
+    // próba wysłania przez res.send liczby generuje błąd serwera, odbiera ją jako kod błędu
+    res.send("result is " + r);
+}
+
 app.listen(3000, function(){console.log("Listening on port 3000 ...");});
 
-app.get("/about", aboutAnswer);
-
-function rootAnswer(req, res){
-    res.sendFile(__dirname + "/index.html");
-}
-
-function aboutAnswer(req, res){
-    res.send("this is my short bio");
-}
