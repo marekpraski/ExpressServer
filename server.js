1df9032781a983b3e6d5fe9a67c6bb76f6cfe575
Marek Praski <marekpraski@prgw.com.pl>
2023-01-10 20:27:12 +0100

    czytam ze strony zewnętrznej przez api i wyświetlam wynik w konsoli; nieudana próba przypisania wyniku do zmiennej globalnej

--- server.js
+++ server.js
@@ -1,28 +1,54 @@
 
 const express = require("express");
+const https = require("https");
 const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
+var j;
+app.get("/", async (req, res) => {
+    console.log("1");
+    getAJoke();
+    console.log("5");
+    res.send( j + "   ha ha ha");});
 
-app.get("/", function(req, res){res.sendFile(__dirname + "/index.html");});
-app.get("/bmiC", function(req, res){res.sendFile(__dirname + "/bmiCalculator.html")});
+//nieudana próba przypisania wartości do globalnej zmiennej j
 
-app.post("/", rootPost);
-app.post("/bmiC", bmiPost);
-
-function rootPost(req, res){
-    var nr1 = Number(req.body.nr1);
-    var nr2 = Number(req.body.nr2);
-    var r = nr1 + nr2;
-    // próba wysłania przez res.send liczby generuje błąd serwera, odbiera ją jako kod błędu
-    res.send("result is " + r);
+async function getAJoke(){
+    console.log("2");
+    https.get("https://v2.jokeapi.dev/joke/Programming", joking);
 }
 
-function bmiPost(req, res){
-    let w = parseFloat(req.body.weight);
-    let h = parseFloat(req.body.heigth);
-    let bmi = Math.round(1000 * w / h) / 1000;
-    res.send("bmi is " + bmi);
+function joking(res){
+    console.log("3");
+    console.log("\n");
+    res.on("data", getData);
+
+    res.on("error", err => {
+        // On error, log to console
+        console.error(`Error: ${err}`);
+    });
+}
+
+function getData(chunk){
+    // On data received, convert it to a JSON object
+    let randomJoke = JSON.parse(chunk.toString());
+    j = randomJoke;
+    console.log(randomJoke);
+    // if(randomJoke.type == "single")
+    // {
+    //     // If type == "single", the joke only has the "joke" property
+    //     console.log(randomJoke.joke);
+    //     console.log("\n");
+    // }
+    // else
+    // {
+    //     // If type == "twopart", the joke has the "setup" and "delivery" properties
+    //     console.log(randomJoke.setup);
+    //     setTimeout(() => {
+    //         console.log(randomJoke.delivery);
+    //         console.log("\n");
+    //     }, 3000);
+    // }
 }
 
 app.listen(3000, function(){console.log("Listening on port 3000 ...");});
